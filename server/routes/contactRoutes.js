44ea98d6f2c6db338bbc860405be42824e0fa23d
Samia-Tabassum <samiatabassum71@gmail.com>
2025-09-02 00:14:20 +0600

    commit 9

---
+++ server/routes/contactRoutes.js
@@ -0,0 +1,8 @@
+import express from "express";
+import { saveMessage } from "../controllers/contactController.js";
+
+const router = express.Router();
+
+router.post("/", saveMessage);
+
+export default router;
