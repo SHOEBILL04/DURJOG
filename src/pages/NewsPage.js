89a3ad18e90b1559c6bbe14f11ee438dff99149a
Samia-Tabassum <samiatabassum71@gmail.com>
2025-09-18 00:07:34 +0600

    css for news

--- src/pages/NewsPage.js
+++ src/pages/NewsPage.js
@@ -1,21 +1,22 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
+import "./NewsPage.css"; 
 
 export default function NewsPage() {
   const [news, setNews] = useState([]);
 
   useEffect(() => {
-    axios.get("http://localhost:5000/api/updates")
+    axios.get("http://localhost:5000/api/updates") 
       .then(res => setNews(res.data))
       .catch(err => console.error(err));
   }, []);
 
   return (
-    <div>
+    <div className="news-container">
       <h2>Latest News</h2>
-      <ul>
+      <ul className="news-list">
         {news.map(item => (
-          <li key={item._id}>
+          <li key={item._id} className="news-item">
             <h3>{item.title}</h3>
             <p>{item.content}</p>
             {item.link && <a href={item.link} target="_blank" rel="noreferrer">Read more</a>}
