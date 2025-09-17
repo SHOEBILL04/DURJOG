dd20fc3aa05a450c884d53b12e3b9bf8e9e69a7d
Samia-Tabassum <samiatabassum71@gmail.com>
2025-09-17 23:21:49 +0600

    news page updated

---
+++ src/pages/NewsPage.js
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+export default function NewsPage() {
+  const [news, setNews] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:5000/api/news")
+      .then(res => setNews(res.data))
+      .catch(err => console.error(err));
+  }, []);
+
+  return (
+    <div>
+      <h2>Latest News</h2>
+      <ul>
+        {news.map(item => (
+          <li key={item._id}>
+            <h3>{item.title}</h3>
+            <p>{item.content}</p>
+            {item.link && <a href={item.link} target="_blank" rel="noreferrer">Read more</a>}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
